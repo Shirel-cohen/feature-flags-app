ce79b0ec5bf2e18bc965414a5d86f7bea1d8bc9c
Shirel-cohen <shireli1234c@gmail.com>
2025-11-06 23:01:35 +0200

    server

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use('/api/flags', flagsRouter);
 
 // Serve React frontend
-const buildPath = path.join(__dirname, '../ui/build');
+const buildPath = path.join(__dirname, '../../ui/build');
 app.use(express.static(buildPath));
 
 // Send index.html for any other route (React handles routing)
